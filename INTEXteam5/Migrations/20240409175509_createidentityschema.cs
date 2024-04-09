49fc79c47015439ee27c057a8d052cb8db63a2f8
sydneygroverr <sydney.grover8@gmail.com>
2024-04-09 13:46:51 -0600

    commit?

---
+++ INTEXteam5/Migrations/20240409175509_createidentityschema.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace INTEXteam5.Migrations
+{
+    /// <inheritdoc />
+    public partial class createidentityschema : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
